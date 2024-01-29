56616bc2c09db428c89ab17b298756937f9d6e7d
kiran6177 <kir4ns6177@gmail.com>
2024-01-29 10:40:29 +0530

    report downloaded

--- controllers/reportController.js
+++ controllers/reportController.js
@@ -1,6 +1,7 @@
 const User = require('../models/userSchema')
 const Order = require('../models/orderSchema')
-const Product = require('../models/productSchema')
+const excelJS = require('exceljs')
+const PDFDocument = require('pdfkit')
 
 const loadReport = async (req,res)=>{
     try {
@@ -360,6 +361,323 @@ const yearlyTurnover = async (req,res)=>{
     }
 }
 
+
+const getExcelData = async (req,res)=>{
+    try {
+        console.log("excel")
+        const {day,month,year,week} = req.query
+        console.log(day,month,year,week)
+        const day1 = day? parseInt(day) : undefined
+        const week1 = week? parseInt(week) : undefined
+        const month1 = month? parseInt(month) : undefined
+        const year1 = year? parseInt(year) : undefined
+        let proSold 
+        if(day!== undefined && month != undefined && year != undefined){
+            
+            const date = new Date(year1,month1-1,day1+1)
+            const start = new Date(date)
+            start.setUTCHours(0,0,0,0)
+            const end = new Date(start)
+            end.setDate(end.getDate()+1)
+            console.log(start,end)
+          proSold = await Order.aggregate([{$match:{payment_status:"Paid",date:{$gte:start,$lt:end}}},{$unwind:'$products'},{$lookup:{from:'users',localField:'user_id',foreignField:'_id',as:'userdetails'}},{$lookup:{from:'products',localField:'products.product_id',foreignField:'_id',as:'prodetails'}},{$sort:{date:-1}}])
+
+        }else if(week!== undefined && month != undefined && year != undefined  && day=== undefined){
+            function getWeekDetails(week)
+        {
+            
+            const firstday = new Date(year1,month1,1)
+            firstday.setUTCHours(0,0,0,0)
+            const startDay = new Date(firstday)
+            startDay.setDate((week - 1)*7 + 1)
+            const endDay = new Date(startDay)
+            if(week1 === 4)
+            {   
+                if(month1 == 0 || month1 == 2 || month1 == 4 || month1 == 6 || month1 == 7 || month1 == 9 || month1 == 11)
+                {
+                    endDay.setDate(startDay.getDate()+9)
+                }
+                else if(month1 == 3 || month1 == 5 || month1 == 8 || month1 == 10){
+                    endDay.setDate(startDay.getDate()+8)
+                }
+                else 
+                {
+                    if(year1%100 !== 0 && year1%4 == 0)
+                    {
+                        endDay.setDate(startDay.getDate()+7)
+                    }
+                    else{
+                        endDay.setDate(startDay.getDate()+6)
+                    }
+                }
+            }
+            else{
+                endDay.setDate(startDay.getDate()+7)
+            }
+        
+            return {startDay,endDay}
+        }
+        const {startDay,endDay} = getWeekDetails(week1)
+        console.log(startDay,endDay)
+        proSold = await Order.aggregate([{$match:{payment_status:"Paid",date:{$gte:startDay,$lt:endDay}}},{$unwind:'$products'},{$lookup:{from:'users',localField:'user_id',foreignField:'_id',as:'userdetails'}},{$lookup:{from:'products',localField:'products.product_id',foreignField:'_id',as:'prodetails'}},{$sort:{date:-1}}])
+        }else if(month != undefined && year != undefined && week=== undefined && day=== undefined){
+            function getMonthDetails(month1)
+        {
+                
+                const date = new Date(year1,month1)
+                date.setUTCDate(1)
+                date.setUTCHours(0,0,0,0)
+                const startDay = new Date(date)
+                let endDay = new Date(startDay)
+                if(month1 == 0 || month1 == 2 || month1 == 4 || month1 == 6 || month1 == 7 || month1 == 9 || month1 == 11)
+                {
+                     endDay.setDate(31)
+                }
+                else if(month1 == 3 || month1 == 5 || month1 == 8 || month1 == 10){
+                     endDay.setDate(30)
+                }
+                else 
+                {
+                    if(year1%100 !== 0 && year1%4 == 0)
+                    {
+                         endDay.setDate(29)
+                    }
+                    else{
+                         endDay.setDate(28)
+                    }
+                }
+   
+            console.log(startDay)
+            console.log(endDay)
+            return {startDay,endDay}
+        }
+        const {startDay,endDay} = getMonthDetails(month1)
+        
+         proSold = await Order.aggregate([{$match:{payment_status:"Paid",date:{$gte:startDay,$lt:endDay}}},{$unwind:'$products'},{$lookup:{from:'users',localField:'user_id',foreignField:'_id',as:'userdetails'}},{$lookup:{from:'products',localField:'products.product_id',foreignField:'_id',as:'prodetails'}},{$sort:{date:-1}}])
+        }else if( year != undefined && week=== undefined && day=== undefined && month == undefined ){
+            function getYearData(year1)
+            {
+                const date = new Date(year1,1)
+                date.setUTCDate(1)
+                date.setUTCHours(0,0,0,0)
+                let startDay = date
+                let endDay = new Date(startDay)
+                endDay.setFullYear(startDay.getFullYear() + 1)
+                console.log(startDay)
+                console.log(endDay)
+                return {startDay,endDay}
+            }
+            const {startDay,endDay} = getYearData(year1)
+        proSold = await Order.aggregate([{$match:{payment_status:"Paid",date:{$gte:startDay,$lt:endDay}}},{$unwind:'$products'},{$lookup:{from:'users',localField:'user_id',foreignField:'_id',as:'userdetails'}},{$lookup:{from:'products',localField:'products.product_id',foreignField:'_id',as:'prodetails'}},{$sort:{date:-1}}])
+
+        }
+
+        // console.log(proSold)
+        const workbook = new excelJS.Workbook()
+        const workSheet = workbook.addWorksheet("Sales Report")
+        workSheet.columns = [
+            {header:"S.no",key:"sno",width:15},
+            {header:"Date Purchased",key:"date",width:15},
+            {header:"Customer Name",key:"name",width:25},
+            {header:"Product Name",key:"pname",width:25},
+            {header:"Quantity",key:"qty",width:15},
+            {header:"Price",key:"price",width:15},
+            {header:"Order Amount",key:"amount",width:15},
+            {header:"Payment Method",key:"method",width:20},
+        ]
+
+        let count = 1
+        let reporter = []
+        proSold.forEach(data=>{
+            let xldata = {sno : count,
+            date: data.date.toString().split(' ')[1] + " " + data.date.toString().split(' ')[2] + " " + data.date.toString().split(' ')[3] ,
+            name: data.userdetails[0].firstname +" "+ data.userdetails[0].lastname,
+            pname: data.prodetails[0].productname,
+            qty:data.products.qty,
+            price:data.prodetails[0].price,
+            amount:data.total_amount,
+            method:data.payment_method
+        }
+            reporter.push(xldata)
+            count++
+        })
+        // console.log(reporter)
+
+        reporter.forEach(rep=>{
+            workSheet.addRow(rep)
+        })
+        workSheet.getRow(1).eachCell((cell)=>{
+            cell.font = {bold:true}
+        })
+
+        res.setHeader(
+            'Content-Type',
+            'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+          );
+          res.setHeader(
+            'Content-Disposition',
+            'attachment; filename=salesreport.xlsx'
+          );
+
+          await workbook.xlsx.write(res);
+          res.end()
+    } catch (error) {
+        console.log(error.message)
+    }
+}
+
+const getPdfData = async (req,res)=>{
+    try {
+        console.log("pdf")
+        const {day,month,year,week,html} = req.query
+        console.log(day,month,year,week)
+        const day1 = day? parseInt(day) : undefined
+        const week1 = week? parseInt(week) : undefined
+        const month1 = month? parseInt(month) : undefined
+        const year1 = year? parseInt(year) : undefined
+        let proSold 
+        if(day!== undefined && month != undefined && year != undefined){
+            
+            const date = new Date(year1,month1-1,day1+1)
+            const start = new Date(date)
+            start.setUTCHours(0,0,0,0)
+            const end = new Date(start)
+            end.setDate(end.getDate()+1)
+            console.log(start,end)
+          proSold = await Order.aggregate([{$match:{payment_status:"Paid",date:{$gte:start,$lt:end}}},{$unwind:'$products'},{$lookup:{from:'users',localField:'user_id',foreignField:'_id',as:'userdetails'}},{$lookup:{from:'products',localField:'products.product_id',foreignField:'_id',as:'prodetails'}},{$sort:{date:-1}}])
+
+        }else if(week!== undefined && month != undefined && year != undefined){
+            function getWeekDetails(week)
+            {
+                
+                const firstday = new Date(year1,month1,1)
+                firstday.setUTCHours(0,0,0,0)
+                const startDay = new Date(firstday)
+                startDay.setDate((week - 1)*7 + 1)
+                const endDay = new Date(startDay)
+                if(week1 === 4)
+                {   
+                    if(month1 == 0 || month1 == 2 || month1 == 4 || month1 == 6 || month1 == 7 || month1 == 9 || month1 == 11)
+                    {
+                        endDay.setDate(startDay.getDate()+9)
+                    }
+                    else if(month1 == 3 || month1 == 5 || month1 == 8 || month1 == 10){
+                        endDay.setDate(startDay.getDate()+8)
+                    }
+                    else 
+                    {
+                        if(year1%100 !== 0 && year1%4 == 0)
+                        {
+                            endDay.setDate(startDay.getDate()+7)
+                        }
+                        else{
+                            endDay.setDate(startDay.getDate()+6)
+                        }
+                    }
+                }
+                else{
+                    endDay.setDate(startDay.getDate()+7)
+                }
+            
+                return {startDay,endDay}
+            }
+            const {startDay,endDay} = getWeekDetails(week1)
+            console.log(startDay,endDay)
+            proSold = await Order.aggregate([{$match:{payment_status:"Paid",date:{$gte:startDay,$lt:endDay}}},{$unwind:'$products'},{$lookup:{from:'users',localField:'user_id',foreignField:'_id',as:'userdetails'}},{$lookup:{from:'products',localField:'products.product_id',foreignField:'_id',as:'prodetails'}},{$sort:{date:-1}}])
+        }else if(month != undefined && year != undefined && week=== undefined && day=== undefined){
+            function getMonthDetails(month1)
+            {
+                    
+                    const date = new Date(year1,month1)
+                    date.setUTCDate(1)
+                    date.setUTCHours(0,0,0,0)
+                    const startDay = new Date(date)
+                    let endDay = new Date(startDay)
+                    if(month1 == 0 || month1 == 2 || month1 == 4 || month1 == 6 || month1 == 7 || month1 == 9 || month1 == 11)
+                    {
+                         endDay.setDate(31)
+                    }
+                    else if(month1 == 3 || month1 == 5 || month1 == 8 || month1 == 10){
+                         endDay.setDate(30)
+                    }
+                    else 
+                    {
+                        if(year1%100 !== 0 && year1%4 == 0)
+                        {
+                             endDay.setDate(29)
+                        }
+                        else{
+                             endDay.setDate(28)
+                        }
+                    }
+       
+                console.log(startDay)
+                console.log(endDay)
+                return {startDay,endDay}
+            }
+            const {startDay,endDay} = getMonthDetails(month1)
+            
+             proSold = await Order.aggregate([{$match:{payment_status:"Paid",date:{$gte:startDay,$lt:endDay}}},{$unwind:'$products'},{$lookup:{from:'users',localField:'user_id',foreignField:'_id',as:'userdetails'}},{$lookup:{from:'products',localField:'products.product_id',foreignField:'_id',as:'prodetails'}},{$sort:{date:-1}}])
+        }else if( year != undefined && week=== undefined && day=== undefined && month == undefined ){
+            function getYearData(year1)
+            {
+                const date = new Date(year1,1)
+                date.setUTCDate(1)
+                date.setUTCHours(0,0,0,0)
+                let startDay = date
+                let endDay = new Date(startDay)
+                endDay.setFullYear(startDay.getFullYear() + 1)
+                console.log(startDay)
+                console.log(endDay)
+                return {startDay,endDay}
+            }
+            const {startDay,endDay} = getYearData(year1)
+        proSold = await Order.aggregate([{$match:{payment_status:"Paid",date:{$gte:startDay,$lt:endDay}}},{$unwind:'$products'},{$lookup:{from:'users',localField:'user_id',foreignField:'_id',as:'userdetails'}},{$lookup:{from:'products',localField:'products.product_id',foreignField:'_id',as:'prodetails'}},{$sort:{date:-1}}])
+        }
+        // console.log(proSold)
+        const doc = new PDFDocument()
+        res.setHeader('Content-disposition', 'attachment; filename=salesreport.pdf');
+        res.setHeader('Content-type', 'application/pdf');
+        doc.pipe(res)
+        doc.fontSize(20).text('Sales Report', { align: 'center' }).moveDown();
+
+        let count = 1
+        let yPos = 100
+        const colomn = 60
+        doc.font('Helvetica-Bold').fontSize(9);
+        doc.text('Serial no.', 30, yPos);
+        doc.text('Date ', 15 + colomn, yPos);
+        doc.text('Customer ', 10 + colomn * 2, yPos);
+        doc.text('Product Name', 10 + colomn * 3, yPos);
+        doc.text('Quantity', 10 + colomn * 5.3, yPos);
+        doc.text('Price', 10 + colomn * 6.2, yPos);
+        doc.text('Total ', 10 + colomn * 7, yPos);
+        doc.text('Payment Method', 10 + colomn * 7.7, yPos);
+        proSold.forEach(data=>{
+            yPos += 20
+            doc.font('Helvetica').fontSize(8);
+            doc.text(count, 30  , yPos);
+            doc.text(data.date.toString().split(' ')[1] + " " + data.date.toString().split(' ')[2] + " " + data.date.toString().split(' ')[3],15 + colomn , yPos);
+            doc.text(data.userdetails[0].firstname +" "+ data.userdetails[0].lastname, 10 + colomn * 2, yPos);
+            doc.text(data.prodetails[0].productname, 10 + colomn * 3, yPos);
+            doc.text(data.products.qty, 10 + colomn * 5.3, yPos);
+            doc.text(data.prodetails[0].price, 10 + colomn * 6.2, yPos);
+            doc.text(data.total_amount, 10 + colomn * 7, yPos);
+            doc.text( data.payment_method, 10 + colomn * 7.7, yPos);
+
+            count++
+            if(count % 30 === 0){
+                doc.addPage();
+                yPos = 50
+            }
+        })
+
+        doc.end();
+    } catch (error) {
+        console.log(error.message)
+    }
+}
+
 module.exports = {
     loadReport,
     dailyProducts,
@@ -369,5 +687,8 @@ module.exports = {
     dailyTurnover,
     weeklyTurnover,
     monthlyTurnover,
-    yearlyTurnover
+    yearlyTurnover,
+    getExcelData,
+    getPdfData
+
 }
